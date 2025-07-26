ff9bdb0a73efd0faa14e069a809dc67d218c033c
Developer-최현수 <gustn9025@naver.com>
2025-07-26 23:03:21 +0900

    init: [최현수] 프로젝트 초기 세팅

---
+++ src/main/java/com/enten/yeogiya/YeogiyaApplication.java
@@ -0,0 +1,13 @@
+package com.enten.yeogiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogiyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeogiyaApplication.class, args);
+    }
+
+}
